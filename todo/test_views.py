9d59b1b2c63703390686415e10ac925ab0fe6d34
mueni2 <anmomoan@gmail.com>
2023-02-17 19:53:44 +0000

    testing

---
+++ todo/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class testDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works2(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works3(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works4(self):
+        self.assertEqual(1, 1)
