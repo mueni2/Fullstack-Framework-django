ca2590a15d9e3fd7779517f55d6fcabb93caf9a8
mueni2 <anmomoan@gmail.com>
2023-02-17 14:07:13 +0000

    models part 1

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=Falese, blank=False)
+    done = models.BooleanField(null=Falese, blank=False, default=False)
