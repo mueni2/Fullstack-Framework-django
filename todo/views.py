c58ef2f8c894c91ddee1533092a370b1a3704329
mueni2 <anmomoan@gmail.com>
2023-02-17 13:23:59 +0000

    templates

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 
 
-def say_hello(request):
-    return HttpResponse('Hello World!')
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
