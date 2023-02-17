34188399fce2dadb210d0f3afa4d4c4eb3659708
mueni2 <anmomoan@gmail.com>
2023-02-17 14:37:58 +0000

    models part 2

--- todo/models.py
+++ todo/models.py
@@ -4,5 +4,9 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=50, null=Falese, blank=False)
-    done = models.BooleanField(null=Falese, blank=False, default=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+
+def __str__(self):
+    return self.name
