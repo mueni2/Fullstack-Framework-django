12091a6e48a2d34bd042fd58bbe776318c8c0a6d
mueni2 <anmomoan@gmail.com>
2023-02-20 12:19:15 +0000

    coverage

--- todo/templates/todo/test_models.py
+++ todo/templates/todo/test_models.py
@@ -7,3 +7,7 @@ class TestModels(TestCase):
     def test_done_default_to_fales(self):
         item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
--- todo/test_views.py
+++ todo/test_views.py
@@ -39,3 +39,10 @@ class testViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item', done=True)
+        response = self.client.get(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
